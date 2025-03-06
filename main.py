78bf84d96573efa4564aa08f05daeb5e8ef91465
Martin Jul <martin@mjul.com>
2025-03-06 23:17:25 +0100

    Configure download for all issues

--- main.py
+++ main.py
@@ -14,14 +14,112 @@ class IssueSource(TypedDict):
 
 
 SOURCES: List[IssueSource] = [
-    {"issue": "1-2", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-1-og-2.pdf"},
+    {"issue": "78",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-78-Muhammedtegninger-Marts-2022.pdf"},
+    {"issue": "77",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2023/10/Libertas-77-Regulering-2-FARVE-November-2021.pdf"},
+    {"issue": "76",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2023/10/Libertas-76-Thomas-Hobbes-2-Juli-2021.pdf"},
+    {"issue": "75",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2023/10/Libertas-75-Frihandel-globalisering-og-protektionisme-FARVE-Marts-2021.pdf"},
+    {"issue": "74",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2023/10/Libertas-74-Har-liberalismen-slaaet-fejl-2-Oktober-2020-1.pdf"},
+    {"issue": "73",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2023/10/Libertas-73-Liberalisme-og-den-franske-revolution-III-juni-2020-FARVE.pdf"},
+    {"issue": "72",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2023/10/Libertas-72-Liberalisme-og-den-franske-revolution-II-FARVE-2-Februar-2020.pdf"},
+    {"issue": "71",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2023/10/Libertas-71-Liberalisme-og-den-franske-revolution-I-FARVE-2-Oktober-2019.pdf"},
+    {"issue": "71-tillæg",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Lancien-regime-Tillæg-til-LIBERTAS.pdf"},
+    {"issue": "70",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-70-Borgerlige-regeringer-juni-2019.pdf"},
+    {"issue": "69",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-69-Indvandring-og-det-åbne-samfund-februar-2019.pdf"},
+    {"issue": "68", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-68-Penge-Oktober-2018.pdf"},
+    {"issue": "67", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-67-Public-choice-Juni-2018.pdf"},
+    {"issue": "66", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-nr-66-2018-De-store-kriser.pdf"},
+    {"issue": "65",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-65-Den-østrigske-skole-Okt-2017.pdf"},
+    {"issue": "64", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-nr-64-Okt-2017-Anarkisme.pdf"},
+    {"issue": "63",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-nr-63-Ytringsfrihed-og-tolerance-II.pdf"},
+    {"issue": "62",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-nr.-62-Ytringsfrihed-og-tolerance-I.pdf"},
+    {"issue": "61",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-nr.-61-Ayn-Rand-og-Adam-Smith-prisen-1.pdf"},
+    {"issue": "60",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-nr.-60-Konservatisme-Trykning-3.pdf"},
+    {"issue": "59",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-nr.-59-25-året-for-Berlinmurens-fald-Trykning-1.pdf"},
+    {"issue": "58", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Lbertas-nr.-58-Nozick-1.pdf"},
+    {"issue": "57",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-57-Frihed-før-liberalismen-II-Revideret-1.pdf"},
+    {"issue": "56",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-56-Frihedsrevolutionerne-1776-1789-1.pdf"},
+    {"issue": "55",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-nr.-55-Liberalisme-Revideret-1.pdf"},
+    {"issue": "54", "uri": "http://www.libertas.dk/arkiv/54.pdf"},
+    {"issue": "53", "uri": "http://www.libertas.dk/arkiv/53.pdf"},
+    {"issue": "52", "uri": "http://www.libertas.dk/arkiv/52.pdf"},
+    {"issue": "51", "uri": "http://www.libertas.dk/arkiv/51.pdf"},
+    {"issue": "50", "uri": "http://www.libertas.dk/arkiv/50.pdf"},
+    {"issue": "49", "uri": "http://www.libertas.dk/arkiv/49.pdf"},
+    {"issue": "48", "uri": "http://www.libertas.dk/arkiv/48.pdf"},
+    {"issue": "47", "uri": "http://www.libertas.dk/arkiv/47.pdf"},
+    {"issue": "45-46", "uri": "http://www.libertas.dk/arkiv/45.pdf"},
+    {"issue": "44", "uri": "http://www.libertas.dk/arkiv/44.pdf"},
+    {"issue": "43", "uri": "http://www.libertas.dk/arkiv/43.pdf"},
+    {"issue": "42", "uri": "http://www.libertas.dk/arkiv/42.pdf"},
+    {"issue": "41", "uri": "http://www.libertas.dk/arkiv/41.pdf"},
+    {"issue": "40", "uri": "http://www.libertas.dk/arkiv/40.pdf"},
+    {"issue": "39", "uri": "http://www.libertas.dk/arkiv/39.pdf"},
+    {"issue": "38", "uri": "http://www.libertas.dk/arkiv/38.pdf"},
+    {"issue": "37", "uri": "http://www.libertas.dk/arkiv/37.pdf"},
+    {"issue": "36", "uri": "http://www.libertas.dk/arkiv/36.pdf"},
+    {"issue": "35", "uri": "http://www.libertas.dk/arkiv/35.pdf"},
+    {"issue": "34", "uri": "http://www.libertas.dk/arkiv/34.pdf"},
+    {"issue": "33", "uri": "http://www.libertas.dk/arkiv/33.pdf"},
+    {"issue": "32", "uri": "http://www.libertas.dk/arkiv/32.pdf"},
+    {"issue": "31", "uri": "http://www.libertas.dk/arkiv/31.pdf"},
+    {"issue": "29-30", "uri": "http://www.libertas.dk/arkiv/29.pdf"},
+    {"issue": "27-28", "uri": "http://www.libertas.dk/arkiv/27.pdf"},
+    {"issue": "26", "uri": "http://www.libertas.dk/arkiv/26.pdf"},
+    {"issue": "25", "uri": "http://www.libertas.dk/arkiv/25.pdf"},
+    {"issue": "24", "uri": "http://www.libertas.dk/arkiv/24.pdf"},
+    {"issue": "23", "uri": "http://www.libertas.dk/arkiv/23.pdf"},
+    {"issue": "22b",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-nr.-22b-Peter-Kurrild-Klitgaard-En-tilfældig-individualists-hændelige-erindringer.pdf"},
+    {"issue": "22", "uri": "http://www.libertas.dk/arkiv/22.pdf"},
+    {"issue": "21", "uri": "http://www.libertas.dk/arkiv/21.pdf"},
+    {"issue": "20", "uri": "http://www.libertas.dk/arkiv/20.pdf"},
+    {"issue": "19", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-19.pdf"},
+    {"issue": "18", "uri": "http://www.libertas.dk/arkiv/18.pdf"},
+    {"issue": "17", "uri": "http://www.libertas.dk/arkiv/17.pdf"},
+    {"issue": "16", "uri": "http://www.libertas.dk/arkiv/16.pdf"},
+    {"issue": "15", "uri": "http://www.libertas.dk/arkiv/15.pdf"},
+    {"issue": "14", "uri": "http://www.libertas.dk/arkiv/14.pdf"},
+    {"issue": "13", "uri": "http://www.libertas.dk/arkiv/13.pdf"},
+    {"issue": "12", "uri": "http://www.libertas.dk/arkiv/12.pdf"},
+    {"issue": "11", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-11-1990.pdf"},
+    {"issue": "10", "uri": "http://www.libertas.dk/arkiv/10.pdf"},
+    {"issue": "9", "uri": "http://www.libertas.dk/arkiv/9.pdf"},
+    {"issue": "8", "uri": "http://www.libertas.dk/arkiv/8.pdf"},
+    {"issue": "7", "uri": "http://www.libertas.dk/arkiv/7.pdf"},
+    {"issue": "6", "uri": "http://www.libertas.dk/arkiv/6.pdf"},
+    {"issue": "5", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-5.pdf"},
+    {"issue": "4", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-4-Friedman-m.m..pdf"},
     {"issue": "3", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-3-Tocqueville.pdf"},
+    {"issue": "1-2", "uri": "http://www.libertas.dk/wp-content/uploads/2016/12/Libertas-1-og-2.pdf"},
+    # Særnumre
+    {"issue": "64-65-konference",
+     "uri": "http://www.libertas.dk/wp-content/uploads/2018/01/Konference-om-anarkisme-og-den-østrigske-skole.pdf"},
 ]
 
 
 def issue_dir(issues_dir: Path, issue_source: IssueSource) -> Path:
     """Get the directory for an issue."""
-    sub_dir = issue_source["issue"].split("-")[0]
+    sub_dir = issue_source["issue"]
     return issues_dir / sub_dir
 
 
@@ -42,9 +140,8 @@ def download_pdfs(issues_dir: Path, sources: List[IssueSource], force):
     for source in sources:
         dir_path = issue_dir(issues_dir, source)
         pdf_path = issue_pdf_path(issues_dir, source)
-        logging.debug(f"Processing {pdf_path}...")
         if pdf_path.exists() and not force:
-            logging.debug(f"Issue {source['issue']} already downloaded. Skipping.")
+            logging.info(f"Issue {source['issue']} already downloaded. Skipping.")
         else:
             dir_path.mkdir(parents=True, exist_ok=True)
             logging.info(f"Downloading issue {source['issue']} from {source['uri']}...")
